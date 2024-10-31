713a36b6dc4e1e9e2ff25ba5e25c8c427e7da6de
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-10-31 04:31:20 -0400

    Time to create an organization step

---
+++ src/forvk/forvk_parameters.f90
@@ -0,0 +1,8 @@
+module forvk_parameters
+  use, intrinsic :: iso_c_binding
+  implicit none
+
+  ! todo: dump all these parameters into forvk.f90
+  !! This is only a temp.
+
+end module forvk_parameters
