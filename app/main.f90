7b5f03195eeff4d9b9b5e1dac2003b2f6beec343
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-10-31 02:01:56 -0400

    Function undefined, time to find it

--- app/main.f90
+++ app/main.f90
@@ -1,7 +1,10 @@
 program main
   use forvk
   use :: glfw
+  use, intrinsic :: iso_c_binding
   implicit none
+
+  integer(c_int), target :: extension_count
 
   if (.not. glfw_init()) then
     error stop "Failed to initialize GLFW."
@@ -12,6 +15,12 @@ program main
   if (.not. glfw_create_window(500, 500, "forvk")) then
     error stop "Failed to create window."
   end if
+
+
+  if (vk_enumerate_instance_extension_properties(c_null_ptr, c_loc(extension_count), c_null_ptr) /= VK_SUCCESS) then
+    print*,"Failed to enumerate extension properties."
+  end if
+
 
   call glfw_destroy_window()
 
