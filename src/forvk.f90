f8f684bbfb999517e4bab2b3c3a7d2855bd726ee
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-10-31 01:25:18 -0400

    space this out

--- src/forvk.f90
+++ src/forvk.f90
@@ -1,10 +1,22 @@
 module forvk
+  use, intrinsic :: iso_c_binding
   implicit none
+
+
   private
 
+
   public :: say_hello
+
+
 contains
+
+
   subroutine say_hello
+    implicit none
+
     print *, "Hello, forvk!"
   end subroutine say_hello
+
+
 end module forvk
