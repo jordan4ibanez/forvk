5ff02135ffe6d4b7917a32d714a26bdde62b6749
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-10-31 01:49:29 -0400

    Add some basics

--- src/forvk.f90
+++ src/forvk.f90
@@ -5,6 +5,16 @@ module forvk
 
   private
 
+  
+  integer(c_int), parameter, public :: VK_MAX_EXTENSION_NAME_SIZE = 256
+
+
+  type, bind(c) :: vk_extension_properties
+    character(len = 1, kind = c_char), dimension(VK_MAX_EXTENSION_NAME_SIZE) :: extension_name
+    !! was uint32_t
+    integer(c_int32_t) :: spec_version
+  end type vk_extension_properties
+
 
 contains
 
