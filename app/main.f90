a76c31cd6389f0d63dd7ca1f580d6d719b401272
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-10-31 01:38:35 -0400

    amazing

--- app/main.f90
+++ app/main.f90
@@ -3,7 +3,15 @@ program main
   use :: glfw
   implicit none
 
-  if (.not. glfw_create_window(500, 500, "forvk")) then
-
+  if (.not. glfw_init()) then
+    error stop "Failed to initialize GLFW."
   end if
+
+  if (.not. glfw_create_window(500, 500, "forvk")) then
+    error stop "Failed to create window."
+  end if
+
+  call glfw_destroy_window()
+
+  call glfw_terminate()
 end program main
