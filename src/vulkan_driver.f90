d370faf91ebb6cfa80aa71a6a4f398bb8c6e83c7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-10-31 10:53:39 -0400

    Basic app info

--- src/vulkan_driver.f90
+++ src/vulkan_driver.f90
@@ -30,6 +30,9 @@ contains
 
     integer(c_int), target :: extension_count
     type(vk_application_info) :: app_info
+    character(len = :, kind = c_char), pointer :: app_name, engine_name
+
+    !? GLFW WINDOW CREATION. =====================================
 
     if (.not. glfw_init()) then
       error stop "[Vulkan] Error: Failed to initialize GLFW."
@@ -43,13 +46,34 @@ contains
       error stop "[Vulkan]: Failed to create window."
     end if
 
+    !? VULKAN CREATION. =====================================
+
     extension_count = 0
 
-    if (vk_enumerate_instance_extension_properties(c_null_ptr, c_loc(extension_count), c_null_ptr) /= VK_SUCCESS) then
-      print*,"Failed to enumerate extension properties."
-    end if
+    ! if (vk_enumerate_instance_extension_properties(c_null_ptr, c_loc(extension_count), c_null_ptr) /= VK_SUCCESS) then
+    !   print*,"Failed to enumerate extension properties."
+    ! end if
 
-    print"(A)","[Vulkan]: "//int_to_string(extension_count)//" extensions supported."
+    ! print"(A)","[Vulkan]: "//int_to_string(extension_count)//" extensions supported."
+
+
+    app_info%s_type = VK_STRUCTURE_TYPE%APPLICATION_INFO
+
+    ! todo: this will need a bit of finesse in the future.
+    allocate(character(len = 10, kind = c_char) :: app_name)
+    app_name = "ForVulkan"//achar(0)
+
+    app_info%p_application_name = c_loc(app_name)
+    app_info%application_version = vk_make_api_version(0,1,0,0)
+
+    ! todo: this will need a bit of finesse in the future.
+    allocate(character(len = 8, kind = c_char) :: engine_name)
+    engine_name = "Formine"//achar(0)
+
+    app_info%p_engine_name = c_loc(engine_name)
+    app_info%engine_version = vk_make_api_version(0,1,0,0)
+
+    app_info%api_version = VK_API_VERSION_1_0
 
 
 
@@ -65,7 +89,6 @@ contains
     end do
 
   end subroutine main_loop
-
 
   subroutine clean_up()
     implicit none
