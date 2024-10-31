9eb9df2dbd7e1eded26e8e8d15ea96c9fa3484f6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-10-31 03:15:16 -0400

    Create vulkan_driver.f90

---
+++ src/vulkan_driver.f90
@@ -0,0 +1,30 @@
+module vulkan_driver
+  use, intrinsic :: iso_c_binding
+  use :: forvk
+  implicit none
+
+
+  ! Treating this thing as a class to follow the vulkan tutorial.
+
+contains
+
+
+  subroutine init_vulkan()
+    implicit none
+
+  end subroutine init_vulkan
+
+
+  subroutine main_loop()
+    implicit none
+
+  end subroutine main_loop
+
+
+  subroutine clean_up()
+    implicit none
+
+  end subroutine clean_up
+
+
+end module vulkan_driver
