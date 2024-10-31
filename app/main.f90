aebd58b0e5feb3c985c5030c491c7be56b00c880
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-10-31 04:15:29 -0400

    and all this

--- app/main.f90
+++ app/main.f90
@@ -1,39 +1,8 @@
 program main
-  use forvk
-  use :: glfw
-  use :: string_f90
-  use :: matrix_4f
-  use :: vector_3f
+  use :: vulkan_driver
   use, intrinsic :: iso_c_binding
   implicit none
 
-  integer(c_int), target :: extension_count
-  type(mat4f) :: matrix
-  type(vec3f) :: vec
+  call vulkan_run()
 
-  
-
-  extension_count = 0
-
-  if (vk_enumerate_instance_extension_properties(c_null_ptr, c_loc(extension_count), c_null_ptr) /= VK_SUCCESS) then
-    print*,"Failed to enumerate extension properties."
-  end if
-
-  print"(A)","[Vulkan]: "//int_to_string(extension_count)//" extensions supported."
-
-  call matrix%identity()
-
-  vec = [1.0, 2.0, 3.0]
-
-  call matrix%translate_vec3f(vec)
-
-
-  do while(.not. glfw_window_should_close())
-    call glfw_poll_events()
-  end do
-
-
-  call glfw_destroy_window()
-
-  call glfw_terminate()
 end program main
--- src/vulkan_driver.f90
+++ src/vulkan_driver.f90
@@ -2,6 +2,7 @@ module vulkan_driver
   use, intrinsic :: iso_c_binding
   use :: forvk
   use :: glfw
+  use :: string_f90
   implicit none
 
 
@@ -10,17 +11,19 @@ module vulkan_driver
 
 contains
 
-  subroutine run()
+  subroutine vulkan_run()
     implicit none
 
     call init_vulkan()
     call main_loop()
     call clean_up()
-  end subroutine run
+  end subroutine vulkan_run
 
 
   subroutine init_vulkan()
     implicit none
+
+    integer(c_int), target :: extension_count
 
     if (.not. glfw_init()) then
       error stop "[Vulkan] Error: Failed to initialize GLFW."
@@ -33,11 +36,23 @@ contains
     if (.not. glfw_create_window(500, 500, "forvk")) then
       error stop "[Vulkan]: Failed to create window."
     end if
+
+    extension_count = 0
+
+    if (vk_enumerate_instance_extension_properties(c_null_ptr, c_loc(extension_count), c_null_ptr) /= VK_SUCCESS) then
+      print*,"Failed to enumerate extension properties."
+    end if
+
+    print"(A)","[Vulkan]: "//int_to_string(extension_count)//" extensions supported."
   end subroutine init_vulkan
 
 
   subroutine main_loop()
     implicit none
+
+    do while(.not. glfw_window_should_close())
+      call glfw_poll_events()
+    end do
 
   end subroutine main_loop
 
@@ -45,6 +60,9 @@ contains
   subroutine clean_up()
     implicit none
 
+    call glfw_destroy_window()
+
+    call glfw_terminate()
   end subroutine clean_up
 
 
