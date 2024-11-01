93dd5d742fb1d49db5a34de1ed70791e503658ef
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-01 05:21:39 -0400

    Revert "Let's try to move the data into fortran"
    This reverts commit f395c4e0d851317a6948aa338b0e7a5ef1695da3.

--- src/forvk/forvk_wrapper.c
+++ src/forvk/forvk_wrapper.c
@@ -1,24 +1,18 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <vulkan/vulkan_core.h>
-#include <stdint.h>
 
-int32_t forvk_wrapper_get_instance_size()
+VkInstance *forvk_grab_instance_pointer()
 {
-  return sizeof(VkInstance);
+  void *data = malloc(sizeof(VkInstance));
+
+  assert(data);
+
+  return (VkInstance *)data;
 }
 
-// VkInstance *forvk_grab_instance_pointer()
-// {
-//   void *data = malloc(sizeof(VkInstance));
-
-//   assert(data);
-
-//   return (VkInstance *)data;
-// }
-
-// void forvk_destroy_pointer_instance(VkInstance *instance)
-// {
-//   // We're not using any custom allocators with this because that would be a nightmare.
-//   vkDestroyInstance(*instance, NULL);
-// }
+void forvk_destroy_pointer_instance(VkInstance *instance)
+{
+  // We're not using any custom allocators with this because that would be a nightmare.
+  vkDestroyInstance(*instance, NULL);
+}
