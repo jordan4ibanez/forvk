f395c4e0d851317a6948aa338b0e7a5ef1695da3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-01 05:10:19 -0400

    Let's try to move the data into fortran

--- src/forvk/forvk_wrapper.c
+++ src/forvk/forvk_wrapper.c
@@ -1,18 +1,24 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <vulkan/vulkan_core.h>
+#include <stdint.h>
 
-VkInstance *forvk_grab_instance_pointer()
+int32_t forvk_wrapper_get_instance_size()
 {
-  void *data = malloc(sizeof(VkInstance));
-
-  assert(data);
-
-  return (VkInstance *)data;
+  return sizeof(VkInstance);
 }
 
-void forvk_destroy_pointer_instance(VkInstance *instance)
-{
-  // We're not using any custom allocators with this because that would be a nightmare.
-  vkDestroyInstance(*instance, NULL);
-}
+// VkInstance *forvk_grab_instance_pointer()
+// {
+//   void *data = malloc(sizeof(VkInstance));
+
+//   assert(data);
+
+//   return (VkInstance *)data;
+// }
+
+// void forvk_destroy_pointer_instance(VkInstance *instance)
+// {
+//   // We're not using any custom allocators with this because that would be a nightmare.
+//   vkDestroyInstance(*instance, NULL);
+// }
