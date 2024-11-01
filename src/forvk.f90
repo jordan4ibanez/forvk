9f61a54efa4c9c1620ac00410e7c693d0c7855c4
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-01 10:04:46 -0400

    fix binding

--- src/forvk.f90
+++ src/forvk.f90
@@ -105,7 +105,8 @@ module forvk
       implicit none
 
       !? char*, uint32_t*, VkExtensionProperties*
-      type(c_ptr), intent(in), value :: p_layer_name, p_property_count, p_properties
+      type(c_ptr), intent(in), value :: p_layer_name, p_properties
+      integer(c_int), intent(inout) :: p_property_count
       !? VkResult.
       integer(c_int) :: vk_result
     end function vk_enumerate_instance_extension_properties
