a72c1e7bcf61ce1b4df517a559aa9cf5419f792c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-10-31 22:09:00 -0400

    Bring in my vector library

--- src/vulkan_driver.f90
+++ src/vulkan_driver.f90
@@ -41,15 +41,13 @@ contains
 
     glfw_extensions = glfw_get_required_instance_extensions(glfw_extension_count)
 
-
-
-
     !? We must grab the raw data pointer from C because it could be
     !? different on different platforms.
     vulkan_instance = vk_grab_instance_pointer()
 
     result = vk_create_instance(c_loc(create_info), c_null_ptr, vulkan_instance)
 
+    
   end subroutine init_vulkan
 
 
