b9550807cfbf71cce60a50c2ebc0f00979d6a1a1
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-10-31 22:01:50 -0400

    create_app_info

--- src/vulkan_driver.f90
+++ src/vulkan_driver.f90
@@ -32,34 +32,13 @@ contains
     ! const char **
     type(c_ptr) :: glfw_extensions
     type(vk_application_info), target :: app_info
-    character(len = :, kind = c_char), pointer :: app_name, engine_name
     type(vk_instance_create_info), target :: create_info
     integer(c_int) :: result
 
-
-    !* VULKAN CREATION. =====================================
     call create_glfw()
 
+    call create_app_info(app_info)
 
-    !? APP INFO. =====================================
-
-
-    app_info%s_type = VK_STRUCTURE_TYPE%APPLICATION_INFO
-
-    ! todo: this will need a bit of finesse in the future.
-    allocate(character(len = 10, kind = c_char) :: app_name)
-    app_name = "ForVulkan"//achar(0)
-
-    app_info%p_application_name = c_loc(app_name)
-    app_info%application_version = vk_make_api_version(0,1,0,0)
-
-    ! todo: this will need a bit of finesse in the future.
-    allocate(character(len = 8, kind = c_char) :: engine_name)
-    engine_name = "Formine"//achar(0)
-
-    app_info%p_engine_name = c_loc(engine_name)
-    app_info%engine_version = vk_make_api_version(0,1,0,0)
-    app_info%api_version = VK_API_VERSION_1_0
 
     !? CREATE INFO. =====================================
 
@@ -97,6 +76,32 @@ contains
       error stop "[Vulkan]: Failed to create window."
     end if
   end subroutine create_glfw
+
+
+  subroutine create_app_info(app_info)
+    implicit none
+
+    character(len = :, kind = c_char), pointer :: app_name, engine_name
+
+    type(vk_application_info), intent(inout), target :: app_info
+
+    app_info%s_type = VK_STRUCTURE_TYPE%APPLICATION_INFO
+
+    ! todo: this will need a bit of finesse in the future.
+    allocate(character(len = 10, kind = c_char) :: app_name)
+    app_name = "ForVulkan"//achar(0)
+
+    app_info%p_application_name = c_loc(app_name)
+    app_info%application_version = vk_make_api_version(0,1,0,0)
+
+    ! todo: this will need a bit of finesse in the future.
+    allocate(character(len = 8, kind = c_char) :: engine_name)
+    engine_name = "Formine"//achar(0)
+
+    app_info%p_engine_name = c_loc(engine_name)
+    app_info%engine_version = vk_make_api_version(0,1,0,0)
+    app_info%api_version = VK_API_VERSION_1_0
+  end subroutine create_app_info
 
 
 !* MAIN LOOP. ====================================================================
