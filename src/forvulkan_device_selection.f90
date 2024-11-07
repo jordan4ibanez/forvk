904ed97de122aecb966cf37fb6e323a2cb550fa7
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-07 05:12:40 -0500

    HNNG

--- src/forvulkan_device_selection.f90
+++ src/forvulkan_device_selection.f90
@@ -1,10 +1,99 @@
 module forvulkan_device_selection
+  use :: forvulkan
   use :: forvulkan_parameters
+  use :: vector
   use, intrinsic :: iso_c_binding
   implicit none
 
 
 contains
+
+
+  subroutine pick_physical_device(vulkan_instance, physical_device)
+    implicit none
+
+    ! VkInstance
+    integer(c_int64_t), intent(in), value :: vulkan_instance
+    ! VkPhysicalDevice
+    integer(c_int64_t), intent(inout) :: physical_device
+    integer(c_int32_t) :: device_count, i
+    ! c_int64_t [VkPhysicalDevice]
+    type(vec) :: available_devices
+    ! VkPhysicalDevice
+    integer(c_int64_t) :: device
+    ! VkPhysicalDevice *
+    integer(c_int64_t), pointer :: device_pointer
+
+    device = 0
+
+    ! First, we will get the available devices from Vulkan.
+    if (vk_enumerate_physical_devices(vulkan_instance, device_count, c_null_ptr) /= VK_SUCCESS) then
+      error stop "[Vulkan] Error: Failed to enumerate physical devices."
+    end if
+
+    if (device_count == 0) then
+      ! todo: in Formine we will make this clean up then run the OpenGL driver. :)
+      error stop "[Vulkan] Error: No GPU with Vulkan support available."
+    end if
+
+    ! Now, create the vector to store the data.
+    available_devices = new_vec(sizeof(device), int(device_count, c_int64_t))
+    call available_devices%resize(int(device_count, c_int64_t), device)
+
+    ! Here we're passing in the underlying C array.
+    if (vk_enumerate_physical_devices(vulkan_instance, device_count, available_devices%get(1_8)) /= VK_SUCCESS) then
+      error stop "[Vulkan] Error: Failed to enumerate physical devices."
+    end if
+
+    ! Let's find a device that's suitable.
+    device_search: do i = 1,int(available_devices%size())
+      ! We're inlining transfering the device pointer from the interal C array into Fortran.
+      call c_f_pointer(available_devices%get(int(i, c_int64_t)), device_pointer)
+
+      ! We found it, woo. That's our physical device.
+      ! todo: Make a menu option to select another physical device.
+      if (device_is_suitable(device_pointer)) then
+        physical_device = device_pointer
+        exit device_search
+      end if
+    end do device_search
+
+    if (physical_device == VK_NULL_HANDLE) then
+      error stop "[Vulkan] Error: No suitable GPU available."
+    end if
+  end subroutine pick_physical_device
+
+
+  function device_is_suitable(device_pointer) result(suitable)
+    implicit none
+
+    ! VkPhysicalDevice
+    integer(c_int64_t), pointer :: device_pointer
+    logical(c_bool) :: suitable
+    type(vk_physical_device_properties), pointer :: device_properties
+    type(vk_physical_device_features), pointer :: device_features
+
+    suitable = .false.
+
+    allocate(device_properties)
+    call vk_get_physical_device_properties(device_pointer, c_loc(device_properties))
+
+    allocate(device_features)
+    call vk_get_physical_device_features(device_pointer, c_loc(device_features))
+
+    ! Check if we have the bare minimum to run with.
+    ! todo: score GPUs.
+    ! todo: device_properties%device_type == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU .and.
+    ! todo: https://vulkan-tutorial.com/Drawing_a_triangle/Setup/Physical_devices_and_queue_families
+    ! todo: Base device suitability checks
+    ! if (device_features%geometry_shader == VK_TRUE) then
+    suitable = .true.
+    ! end if
+
+    deallocate(device_properties)
+    deallocate(device_features)
+
+  end function device_is_suitable
 
 
 end module forvulkan_device_selection
