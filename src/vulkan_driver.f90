bf7547967d0905dee0bc4998e610e03340e123d0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-08 08:02:11 -0500

    Now break this off and organize

--- src/vulkan_driver.f90
+++ src/vulkan_driver.f90
@@ -12,6 +12,7 @@ module vulkan_driver
   use :: vulkan_driver_create_instance
   use :: vulkan_driver_create_debug_messenger
   use :: vulkan_driver_device_selection
+  use :: vulkan_driver_create_logical_device
   implicit none
 
   ! https://github.com/KhronosGroup/Vulkan-Headers/blob/main/include/vulkan/vulkan_core.h
@@ -96,7 +97,7 @@ contains
 
     call select_physical_device(vulkan_instance, physical_device, queue_index)
 
-    call create_logical_device(queue_index, required_validation_layers)
+    call create_logical_device(physical_device, logical_device, queue_index, required_validation_layers, DEBUG_MODE)
 
     ! todo: deallocate any pointers inside.
     deallocate(app_info)
@@ -105,50 +106,7 @@ contains
   end subroutine init_vulkan
 
 
-  subroutine create_logical_device(queue_index, required_validation_layers)
-    implicit none
 
-    type(forvulkan_queue_family_index), intent(in) :: queue_index
-    ! const char **
-    type(vec), intent(inout) :: required_validation_layers
-    type(vk_device_queue_create_info), pointer :: queue_create_info
-    real(c_float), target :: queue_priority
-    type(vk_physical_device_features), pointer :: device_features
-    type(vk_device_create_info), pointer :: device_create_info
-
-    allocate(queue_create_info)
-
-    queue_create_info%s_type = VK_STRUCTURE_TYPE%DEVICE%QUEUE_CREATE_INFO
-    queue_create_info%queue_family_index = queue_index%graphics_family
-    queue_create_info%queue_count = 1
-
-    queue_priority = 1.0
-    queue_create_info%p_queue_priorities = c_loc(queue_priority)
-
-    allocate(device_features)
-    ! Leaving device_features at defaults for now.
-
-    allocate(device_create_info)
-
-    device_create_info%s_type = VK_STRUCTURE_TYPE%DEVICE%CREATE_INFO
-    device_create_info%p_queue_create_infos = c_loc(queue_create_info)
-    device_create_info%queue_create_info_count = 1
-    device_create_info%p_enabled_features = c_loc(device_features)
-
-    device_create_info%enabled_extension_count = 0
-
-    if (DEBUG_MODE) then
-      device_create_info%enabled_layer_count = int(required_validation_layers%size())
-      ! Passing in the underlying C array.
-      device_create_info%pp_enabled_layer_names = required_validation_layers%get(1_8)
-    else
-      device_create_info%enabled_layer_count = 0
-    end if
-
-    if (vk_create_device(physical_device, c_loc(device_create_info), c_null_ptr, c_loc(logical_device)) /= VK_SUCCESS) then
-      error stop "[Vulkan]: Failed to create logical device."
-    end if
-  end subroutine create_logical_device
 
 
 
---
+++ src/vulkan_driver_steps/vulkan_driver_create_logical_device.f90
@@ -0,0 +1,61 @@
+module vulkan_driver_create_logical_device
+  use, intrinsic :: iso_c_binding
+  use :: forvulkan
+  use :: forvulkan_parameters
+  use :: vector
+  implicit none
+
+
+contains
+
+
+  subroutine create_logical_device(physical_device, logical_device, queue_index, required_validation_layers, DEBUG_MODE)
+    implicit none
+
+    integer(c_int64_t), intent(in), value :: physical_device
+    integer(c_int64_t), intent(inout), target :: logical_device
+    type(forvulkan_queue_family_index), intent(in) :: queue_index
+    ! const char **
+    type(vec), intent(inout) :: required_validation_layers
+    logical(c_bool), intent(in), value :: DEBUG_MODE
+    type(vk_device_queue_create_info), pointer :: queue_create_info
+    real(c_float), target :: queue_priority
+    type(vk_physical_device_features), pointer :: device_features
+    type(vk_device_create_info), pointer :: device_create_info
+
+    allocate(queue_create_info)
+
+    queue_create_info%s_type = VK_STRUCTURE_TYPE%DEVICE%QUEUE_CREATE_INFO
+    queue_create_info%queue_family_index = queue_index%graphics_family
+    queue_create_info%queue_count = 1
+
+    queue_priority = 1.0
+    queue_create_info%p_queue_priorities = c_loc(queue_priority)
+
+    allocate(device_features)
+    ! Leaving device_features at defaults for now.
+
+    allocate(device_create_info)
+
+    device_create_info%s_type = VK_STRUCTURE_TYPE%DEVICE%CREATE_INFO
+    device_create_info%p_queue_create_infos = c_loc(queue_create_info)
+    device_create_info%queue_create_info_count = 1
+    device_create_info%p_enabled_features = c_loc(device_features)
+
+    device_create_info%enabled_extension_count = 0
+
+    if (DEBUG_MODE) then
+      device_create_info%enabled_layer_count = int(required_validation_layers%size())
+      ! Passing in the underlying C array.
+      device_create_info%pp_enabled_layer_names = required_validation_layers%get(1_8)
+    else
+      device_create_info%enabled_layer_count = 0
+    end if
+
+    if (vk_create_device(physical_device, c_loc(device_create_info), c_null_ptr, c_loc(logical_device)) /= VK_SUCCESS) then
+      error stop "[Vulkan]: Failed to create logical device."
+    end if
+  end subroutine create_logical_device
+
+
+end module vulkan_driver_create_logical_device
