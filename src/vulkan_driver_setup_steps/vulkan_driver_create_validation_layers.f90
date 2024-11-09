af0598f7c0666905a87a13650409f9a5f07ea3fe
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-09 13:56:55 -0500

    This was moved

--- src/vulkan_driver_setup_steps/vulkan_driver_create_validation_layers.f90
+++ src/vulkan_driver_setup_steps/vulkan_driver_create_validation_layers.f90
@@ -10,32 +10,6 @@ module vulkan_driver_create_validation_layers
 contains
 
 
-  subroutine create_required_validation_layers(required_validation_layers, DEBUG_MODE)
-    implicit none
-
-    ! const char *
-    type(vec), intent(inout) :: required_validation_layers
-    logical(c_bool), intent(in), value :: DEBUG_MODE
-    character(len = :, kind = c_char), pointer :: layer_name
-
-    ! If we're not in debug mode, don't bother with this.
-    if (.not. DEBUG_MODE) then
-      return
-    end if
-
-    ! Create validation layers that we need.
-
-    print"(A)","[Vulkan]: Creating required validation layers."
-
-    layer_name => null()
-    required_validation_layers = new_vec(sizeof(c_null_ptr), 0_8)
-
-    allocate(character(len = 28, kind = c_char) :: layer_name)
-    layer_name = "VK_LAYER_KHRONOS_validation"//achar(0)
-    call required_validation_layers%push_back(c_loc(layer_name))
-  end subroutine create_required_validation_layers
-
-
   subroutine ensure_validation_layer_support(required_validation_layers, DEBUG_MODE)
     implicit none
 
