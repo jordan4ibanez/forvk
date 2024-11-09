0dd0dded7274b6d72df64938732bde2fb9c440e8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-09 14:10:18 -0500

    rename this

--- src/vulkan_driver_setup_steps/vulkan_driver_create_debug_messenger.f90
+++ src/vulkan_driver_setup_steps/vulkan_driver_create_debug_messenger.f90
@@ -9,7 +9,7 @@ module vulkan_driver_create_debug_messenger
 contains
 
 
-  subroutine create_messenger_struct(messenger_create_info, DEBUG_MODE)
+  subroutine create_debug_messenger_struct(messenger_create_info, DEBUG_MODE)
     implicit none
 
     type(vk_debug_utils_messenger_create_info_ext), intent(inout), target :: messenger_create_info!validation_create_info
@@ -24,7 +24,7 @@ contains
     messenger_create_info%message_type = or(VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT, or(VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT, VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT))
     messenger_create_info%pfn_user_callback = c_funloc(debug_callback)
     messenger_create_info%p_user_data = c_null_ptr ! Optional.
-  end subroutine create_messenger_struct
+  end subroutine create_debug_messenger_struct
 
 
   subroutine setup_debug_messenger(debug_messenger_create_info, vulkan_instance, debug_messenger, DEBUG_MODE)
@@ -43,7 +43,7 @@ contains
     print"(A)","[Vulkan]: Setting up debug messenger."
 
     allocate(debug_messenger_create_info)
-    call create_messenger_struct(debug_messenger_create_info, DEBUG_MODE)
+    call create_debug_messenger_struct(debug_messenger_create_info, DEBUG_MODE)
 
     if (forvulkan_create_debug_utils_messenger_ext(vulkan_instance, c_loc(debug_messenger_create_info), c_null_ptr, debug_messenger) /= VK_SUCCESS) then
       error stop "[Vulkan] Error: Failed to set up debug messenger."
