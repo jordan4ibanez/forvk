32e14e243b41635cca25f994610dde062e5659e5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-09 07:03:30 -0500

    Make this understandable

--- src/vulkan_driver_steps/vulkan_driver_create_logical_device.f90
+++ src/vulkan_driver_steps/vulkan_driver_create_logical_device.f90
@@ -21,8 +21,8 @@ contains
     logical(c_bool), intent(in), value :: DEBUG_MODE
     type(vk_device_queue_create_info), pointer :: queue_create_info
     real(c_float), target :: queue_priority
-    type(vk_physical_device_features), pointer :: device_features
-    type(vk_device_create_info), pointer :: device_create_info
+    type(vk_physical_device_features), pointer :: physical_device_features
+    type(vk_device_create_info), pointer :: logical_device_create_info
 
     allocate(queue_create_info)
 
@@ -33,27 +33,27 @@ contains
     queue_priority = 1.0
     queue_create_info%p_queue_priorities = c_loc(queue_priority)
 
-    allocate(device_features)
+    allocate(physical_device_features)
     ! Leaving device_features at defaults for now.
 
-    allocate(device_create_info)
+    allocate(logical_device_create_info)
 
-    device_create_info%s_type = VK_STRUCTURE_TYPE%DEVICE%CREATE_INFO
-    device_create_info%p_queue_create_infos = c_loc(queue_create_info)
-    device_create_info%queue_create_info_count = 1
-    device_create_info%p_enabled_features = c_loc(device_features)
+    logical_device_create_info%s_type = VK_STRUCTURE_TYPE%DEVICE%CREATE_INFO
+    logical_device_create_info%p_queue_create_infos = c_loc(queue_create_info)
+    logical_device_create_info%queue_create_info_count = 1
+    logical_device_create_info%p_enabled_features = c_loc(physical_device_features)
 
-    device_create_info%enabled_extension_count = 0
+    logical_device_create_info%enabled_extension_count = 0
 
     if (DEBUG_MODE) then
-      device_create_info%enabled_layer_count = int(required_validation_layers%size())
+      logical_device_create_info%enabled_layer_count = int(required_validation_layers%size())
       ! Passing in the underlying C array.
-      device_create_info%pp_enabled_layer_names = required_validation_layers%get(1_8)
+      logical_device_create_info%pp_enabled_layer_names = required_validation_layers%get(1_8)
     else
-      device_create_info%enabled_layer_count = 0
+      logical_device_create_info%enabled_layer_count = 0
     end if
 
-    if (vk_create_device(physical_device, c_loc(device_create_info), c_null_ptr, c_loc(logical_device)) /= VK_SUCCESS) then
+    if (vk_create_device(physical_device, c_loc(logical_device_create_info), c_null_ptr, c_loc(logical_device)) /= VK_SUCCESS) then
       error stop "[Vulkan]: Failed to create logical device."
     end if
 
