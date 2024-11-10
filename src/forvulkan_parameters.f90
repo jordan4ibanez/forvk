c955cf4c37661ba62233648fa6c88514168ec9ef
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-10 03:21:27 -0500

    VkColorSpaceKHR

--- src/forvulkan_parameters.f90
+++ src/forvulkan_parameters.f90
@@ -663,6 +663,48 @@ module forvulkan_parameters
   integer(c_int), parameter, public :: VK_FORMAT_R16G16_S10_5_NV = VK_FORMAT_R16G16_SFIXED5_NV
 
 
+  !? VkColorSpaceKHR.
+
+
+  integer(c_int), parameter, public :: VK_COLOR_SPACE_SRGB_NONLINEAR_KHR = 0
+  ! Provided by VK_EXT_swapchain_colorspace
+  integer(c_int), parameter, public :: VK_COLOR_SPACE_DISPLAY_P3_NONLINEAR_EXT = 1000104001
+  ! Provided by VK_EXT_swapchain_colorspace
+  integer(c_int), parameter, public :: VK_COLOR_SPACE_EXTENDED_SRGB_LINEAR_EXT = 1000104002
+  ! Provided by VK_EXT_swapchain_colorspace
+  integer(c_int), parameter, public :: VK_COLOR_SPACE_DISPLAY_P3_LINEAR_EXT = 1000104003
+  ! Provided by VK_EXT_swapchain_colorspace
+  integer(c_int), parameter, public :: VK_COLOR_SPACE_DCI_P3_NONLINEAR_EXT = 1000104004
+  ! Provided by VK_EXT_swapchain_colorspace
+  integer(c_int), parameter, public :: VK_COLOR_SPACE_BT709_LINEAR_EXT = 1000104005
+  ! Provided by VK_EXT_swapchain_colorspace
+  integer(c_int), parameter, public :: VK_COLOR_SPACE_BT709_NONLINEAR_EXT = 1000104006
+  ! Provided by VK_EXT_swapchain_colorspace
+  integer(c_int), parameter, public :: VK_COLOR_SPACE_BT2020_LINEAR_EXT = 1000104007
+  ! Provided by VK_EXT_swapchain_colorspace
+  integer(c_int), parameter, public :: VK_COLOR_SPACE_HDR10_ST2084_EXT = 1000104008
+  ! Provided by VK_EXT_swapchain_colorspace
+  ! VK_COLOR_SPACE_DOLBYVISION_EXT is deprecated but no reason was given in the API XML
+  integer(c_int), parameter, public :: VK_COLOR_SPACE_DOLBYVISION_EXT = 1000104009
+  ! Provided by VK_EXT_swapchain_colorspace
+  integer(c_int), parameter, public :: VK_COLOR_SPACE_HDR10_HLG_EXT = 1000104010
+  ! Provided by VK_EXT_swapchain_colorspace
+  integer(c_int), parameter, public :: VK_COLOR_SPACE_ADOBERGB_LINEAR_EXT = 1000104011
+  ! Provided by VK_EXT_swapchain_colorspace
+  integer(c_int), parameter, public :: VK_COLOR_SPACE_ADOBERGB_NONLINEAR_EXT = 1000104012
+  ! Provided by VK_EXT_swapchain_colorspace
+  integer(c_int), parameter, public :: VK_COLOR_SPACE_PASS_THROUGH_EXT = 1000104013
+  ! Provided by VK_EXT_swapchain_colorspace
+  integer(c_int), parameter, public :: VK_COLOR_SPACE_EXTENDED_SRGB_NONLINEAR_EXT = 1000104014
+  ! Provided by VK_AMD_display_native_hdr
+  integer(c_int), parameter, public :: VK_COLOR_SPACE_DISPLAY_NATIVE_AMD = 1000213000
+  ! VK_COLORSPACE_SRGB_NONLINEAR_KHR is a deprecated alias
+  integer(c_int), parameter, public :: VK_COLORSPACE_SRGB_NONLINEAR_KHR = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR
+  ! Provided by VK_EXT_swapchain_colorspace
+  ! VK_COLOR_SPACE_DCI_P3_LINEAR_EXT is a deprecated alias
+  integer(c_int), parameter, public :: VK_COLOR_SPACE_DCI_P3_LINEAR_EXT = VK_COLOR_SPACE_DISPLAY_P3_LINEAR_EXT
+
+
   !? VkStructureType. ==========================================================================
 
 
