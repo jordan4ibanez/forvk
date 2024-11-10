cb9f5750a36b2a5640436018be22f5b65871b991
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-10 01:01:24 -0500

    Breaking this out into a function fixed it :)

--- src/vulkan_driver_helpers/vulkan_driver_create_physical_device_extensions.f90
+++ src/vulkan_driver_helpers/vulkan_driver_create_physical_device_extensions.f90
@@ -18,16 +18,13 @@ contains
     ! character **
     type(vec), intent(inout) :: required_device_extensions
     character(len = :, kind = c_char), pointer :: required_extension
-    type(c_ptr) :: raw_c_ptr
 
     required_device_extensions = new_vec(sizeof(c_null_ptr), 0_8)
 
     allocate(character(len = len(VK_KHR_SWAPCHAIN_EXTENSION_NAME), kind = c_char) :: required_extension)
     required_extension = VK_KHR_SWAPCHAIN_EXTENSION_NAME
 
-    ! This is done like this so we don't blow up gfortran.
-    raw_c_ptr = c_loc(required_extension)
-    call required_device_extensions%push_back(raw_c_ptr)
+    call required_device_extensions%push_back(c_loc(required_extension))
 
     call required_device_extensions%shrink_to_fit()
   end subroutine create_required_physical_device_extensions
