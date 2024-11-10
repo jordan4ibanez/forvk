e9e72b2b822d9f41e74d0417fcbbc823ef120647
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-10 03:28:40 -0500

    VkPresentModeKHR

--- src/forvulkan_parameters.f90
+++ src/forvulkan_parameters.f90
@@ -705,6 +705,21 @@ module forvulkan_parameters
   integer(c_int), parameter, public :: VK_COLOR_SPACE_DCI_P3_LINEAR_EXT = VK_COLOR_SPACE_DISPLAY_P3_LINEAR_EXT
 
 
+  !? VkPresentModeKHR.
+
+
+  integer(c_int), parameter, public :: VK_PRESENT_MODE_IMMEDIATE_KHR = 0
+  integer(c_int), parameter, public :: VK_PRESENT_MODE_MAILBOX_KHR = 1
+  integer(c_int), parameter, public :: VK_PRESENT_MODE_FIFO_KHR = 2
+  integer(c_int), parameter, public :: VK_PRESENT_MODE_FIFO_RELAXED_KHR = 3
+  ! Provided by VK_KHR_shared_presentable_image
+  integer(c_int), parameter, public :: VK_PRESENT_MODE_SHARED_DEMAND_REFRESH_KHR = 1000111000
+  ! Provided by VK_KHR_shared_presentable_image
+  integer(c_int), parameter, public :: VK_PRESENT_MODE_SHARED_CONTINUOUS_REFRESH_KHR = 1000111001
+  ! Provided by VK_EXT_present_mode_fifo_latest_ready
+  integer(c_int), parameter, public :: VK_PRESENT_MODE_FIFO_LATEST_READY_EXT = 1000361000
+
+
   !? VkStructureType. ==========================================================================
 
 
