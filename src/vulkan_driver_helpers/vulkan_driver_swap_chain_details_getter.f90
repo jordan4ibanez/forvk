3f5ac7bf355f32186cfef2e105e64dc29fa23f8b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-11 01:53:00 -0500

    ease of use function

---
+++ src/vulkan_driver_helpers/vulkan_driver_swap_chain_details_getter.f90
@@ -0,0 +1,13 @@
+module vulkan_driver_swap_chain_details_getter
+  use, intrinsic :: iso_c_binding
+  use :: vulkan_driver
+  implicit none
+
+
+contains
+
+
+  
+
+
+end module vulkan_driver_swap_chain_details_getter
