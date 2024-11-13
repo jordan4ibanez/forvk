038c9dcd535f8fcd8b72c5582047ce5876b906d3
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-13 09:57:40 -0500

    shaderc_include_result

--- src/vulkan_shader_compiler/shaderc_bindings.f90
+++ src/vulkan_shader_compiler/shaderc_bindings.f90
@@ -14,7 +14,28 @@ module shaderc_bindings
   integer(c_int32_t), parameter :: shaderc_glsl_fragment_shader = 1
 
 
+  !? shaderc_include_result.
+  type, bind(c) :: shaderc_include_result
+    ! The name of the source file.  The name should be fully resolved
+    ! in the sense that it should be a unique name in the context of the
+    ! includer.  For example, if the includer maps source names to files in
+    ! a filesystem, then this name should be the absolute path of the file.
+    ! For a failed inclusion, this string is empty.
+    ! const char*
+    type(c_ptr) :: source_name
+    integer(c_size_t) :: source_name_length
+    ! The text contents of the source file in the normal case.
+    ! For a failed inclusion, this contains the error message.
+    ! const char*
+    type(c_ptr) :: content
+    integer(c_size_t) :: content_length
+    ! User data to be passed along with this request.
+    ! void*
+    type(c_ptr) :: user_data
+  end type shaderc_include_result
+
   interface
+
 
     function shaderc_compile_options_initialize() result(shader_options_pointer) bind(c, name = "shaderc_compile_options_initialize")
       use, intrinsic :: iso_c_binding
