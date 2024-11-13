53dfbebd99b6d9dd81f5c4513b21392ede351cc8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-13 03:39:10 -0500

    VkImageViewType

--- src/forvulkan_parameters.f90
+++ src/forvulkan_parameters.f90
@@ -748,6 +748,18 @@ module forvulkan_parameters
   integer(c_int), parameter, public :: VK_COMPONENT_SWIZZLE_A = 6
 
 
+  !? VkImageViewType.
+
+
+  integer(c_int), parameter, public :: VK_IMAGE_VIEW_TYPE_1D = 0
+  integer(c_int), parameter, public :: VK_IMAGE_VIEW_TYPE_2D = 1
+  integer(c_int), parameter, public :: VK_IMAGE_VIEW_TYPE_3D = 2
+  integer(c_int), parameter, public :: VK_IMAGE_VIEW_TYPE_CUBE = 3
+  integer(c_int), parameter, public :: VK_IMAGE_VIEW_TYPE_1D_ARRAY = 4
+  integer(c_int), parameter, public :: VK_IMAGE_VIEW_TYPE_2D_ARRAY = 5
+  integer(c_int), parameter, public :: VK_IMAGE_VIEW_TYPE_CUBE_ARRAY = 6
+
+
   !? VkStructureType. ==========================================================================
 
 
