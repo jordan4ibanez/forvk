f44b44425e4013cc070fddd51cf982133fd50c0a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-13 08:16:45 -0500

    We will do this functionally

--- src/vulkan_shader_compiler/vulkan_shader_compiler.f90
+++ src/vulkan_shader_compiler/vulkan_shader_compiler.f90
@@ -7,4 +7,14 @@ module vulkan_shader_compiler
 contains
 
 
+  subroutine compile_glsl_shaders()
+    implicit none
+
+    type(c_ptr) :: shader_compiler_pointer
+
+    shader_compiler_pointer = shaderc_compiler_initialize()
+
+  end subroutine compile_glsl_shaders
+
+
 end module vulkan_shader_compiler
