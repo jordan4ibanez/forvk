b429e151fe0732aa3a1e0536c198c9dacbe26951
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-13 08:30:13 -0500

    Get them shader dirs

--- src/vulkan_shader_compiler/vulkan_shader_compiler.f90
+++ src/vulkan_shader_compiler/vulkan_shader_compiler.f90
@@ -1,6 +1,7 @@
 module vulkan_shader_compiler
   use, intrinsic :: iso_c_binding
   use :: shaderc_bindings
+  use :: directory
   implicit none
 
 
@@ -11,10 +12,17 @@ contains
     implicit none
 
     type(c_ptr) :: shader_compiler_pointer
+    type(directory_reader) :: reader
+    integer(c_int32_t) :: i
 
     shader_compiler_pointer = shaderc_compiler_initialize()
 
 
+    call reader%read_directory("./shaders/")
+
+    do i = 1,reader%file_count
+      print*,reader%files(i)
+    end do
 
     call shaderc_compiler_release(shader_compiler_pointer)
   end subroutine compile_glsl_shaders
