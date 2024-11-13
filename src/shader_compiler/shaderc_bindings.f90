607836252dd98ecfcaf6442b92267602318e8e63
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-13 08:03:14 -0500

    credit where credit is due

--- src/shader_compiler/shaderc_bindings.f90
+++ src/shader_compiler/shaderc_bindings.f90
@@ -2,4 +2,9 @@ module shaderc_bindings
   use, intrinsic :: iso_c_binding
   implicit none
 
+  ! I did not figure this out on my own.
+  ! https://www.reddit.com/r/vulkan/comments/ecnjn7/converting_shader_source_to_spirv_on_the_fly/
+  ! https://github.com/jbikker/lighthouse2/blob/master/lib/rendercore_vulkan_rt/vulkan_shader.cpp
+  ! https://github.com/google/shaderc/blob/main/libshaderc/include/shaderc/shaderc.h
+
 end module shaderc_bindings
