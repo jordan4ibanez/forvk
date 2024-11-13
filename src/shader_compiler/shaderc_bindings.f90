b149afdf533d82ab6010ee91d80a57322e3cca69
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-13 08:05:27 -0500

    take a complete shot in the dark
    while I'm pulling in google's spirv compiler

--- src/shader_compiler/shaderc_bindings.f90
+++ src/shader_compiler/shaderc_bindings.f90
@@ -7,4 +7,16 @@ module shaderc_bindings
   ! https://github.com/jbikker/lighthouse2/blob/master/lib/rendercore_vulkan_rt/vulkan_shader.cpp
   ! https://github.com/google/shaderc/blob/main/libshaderc/include/shaderc/shaderc.h
 
+  interface
+
+    function shaderc_compile_into_spv() result(unknown) bind(c, name = "shaderc_compile_into_spv")
+      use, intrinsic :: iso_c_binding
+      implicit none
+
+      integer(c_int32_t) :: unknown
+    end function shaderc_compile_into_spv
+
+  end interface
+
+
 end module shaderc_bindings
