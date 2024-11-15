ad443411aeb6d86d865412ab2e6ada136d22a920
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-15 03:48:44 -0500

    document pipeline creation steps

--- src/vulkan_driver_setup_steps/vulkan_driver_create_graphics_pipeline.f90
+++ src/vulkan_driver_setup_steps/vulkan_driver_create_graphics_pipeline.f90
@@ -24,31 +24,39 @@ contains
     ! VkDynamicState
     integer(c_int32_t), dimension(2), target :: dynamic_states
     type(vk_pipeline_dynamic_state_create_info) :: dynamic_state_create_info
+    type(vk_pipeline_vertex_input_state_create_info) :: vertex_input_create_info
 
+    ! First compile GLSL into shader modules.
     vertex_shader_module = compile_glsl_shaders(logical_device, "vertex.vert")
     fragment_shader_module = compile_glsl_shaders(logical_device, "fragment.frag")
 
+    ! Next, Create the vertex shader stage info.
     vertex_shader_stage_info%s_type = VK_STRUCTURE_TYPE%PIPELINE%SHADER_STAGE_CREATE_INFO
     vertex_shader_stage_info%stage = VK_SHADER_STAGE_VERTEX_BIT
     vertex_shader_stage_info%module = vertex_shader_module
     vert_p_name = "main"//achar(0)
     vertex_shader_stage_info%p_name = c_loc(vert_p_name)
 
+    ! Then, create the fragment shader stage create info.
     fragment_shader_stage_info%s_type = VK_STRUCTURE_TYPE%PIPELINE%SHADER_STAGE_CREATE_INFO
     fragment_shader_stage_info%stage = VK_SHADER_STAGE_FRAGMENT_BIT
     fragment_shader_stage_info%module = fragment_shader_module
     frag_p_name = "main"//achar(0)
     fragment_shader_stage_info%p_name = c_loc(frag_p_name)
 
+    ! Put them into an array.
     shader_stages = [vertex_shader_stage_info, fragment_shader_stage_info]
 
+    ! Set up dynamic states for the pipeline.
     dynamic_states = [VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_SCISSOR]
 
     dynamic_state_create_info%s_type = VK_STRUCTURE_TYPE%PIPELINE%DYNAMIC_STATE_CREATE_INFO
     dynamic_state_create_info%dynamic_state_count = size(dynamic_states)
     dynamic_state_create_info%p_dynamic_states = c_loc(dynamic_states)
 
-    
+    ! Create vertex input create info.
+
+
 
 
     call vk_destroy_shader_module(logical_device, fragment_shader_module, c_null_ptr)
