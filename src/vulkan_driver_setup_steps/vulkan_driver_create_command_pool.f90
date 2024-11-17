3e636a37ea748f72b2cb1ace3324871ec44f29b5
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-17 13:58:18 -0500

    Create the command pool

--- src/vulkan_driver_setup_steps/vulkan_driver_create_command_pool.f90
+++ src/vulkan_driver_setup_steps/vulkan_driver_create_command_pool.f90
@@ -9,17 +9,29 @@ module vulkan_driver_create_command_pool
 contains
 
 
-  subroutine create_command_pool(physical_device)
+  subroutine create_command_pool(physical_device, window_surface, logical_device, command_pool)
     implicit none
 
     ! VkPhysicalDevice
     integer(c_int64_t), intent(in), value :: physical_device
+    ! VkSurfaceKHR
+    integer(c_int64_t), intent(in), value :: window_surface
+    ! VkDevice
+    integer(c_int64_t), intent(in), value :: logical_device
+    ! VkCommandPool
+    integer(c_int64_t), intent(inout) :: command_pool
     type(forvulkan_queue_family_indices) :: queue_family_indices
+    type(vk_command_pool_create_info), target :: command_pool_create_info
 
+    queue_family_indices = find_queue_families(physical_device, window_surface)
 
+    command_pool_create_info%s_type = VK_STRUCTURE_TYPE%COMMAND_POOL_CREATE_INFO
+    command_pool_create_info%flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT
+    command_pool_create_info%queue_family_index = queue_family_indices%graphics_family
 
-
-
+    if (vk_create_command_pool(logical_device, c_loc(command_pool_create_info), c_null_ptr, command_pool) /= VK_SUCCESS) then
+      error stop "[Vulkan] Error: Failed to create command pool."
+    end if
   end subroutine create_command_pool
 
 
