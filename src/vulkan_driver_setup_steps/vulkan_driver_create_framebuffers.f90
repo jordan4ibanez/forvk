92e9b9b4b3a31fd324e4a75a876c302531b93cbd
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-17 12:52:03 -0500

    plural

---
+++ src/vulkan_driver_setup_steps/vulkan_driver_create_framebuffers.f90
@@ -0,0 +1,16 @@
+module vulkan_driver_create_framebuffers
+  use, intrinsic :: iso_c_binding
+  use :: forvulkan
+  use :: forvulkan_parameters
+  implicit none
+
+
+contains
+
+
+  subroutine create_framebuffers()
+
+  end subroutine create_framebuffers
+
+
+end module vulkan_driver_create_framebuffers
