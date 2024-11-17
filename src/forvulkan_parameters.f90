113855a2991265799ec2bd191e307408d86904fb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-17 03:39:52 -0500

    VkAttachmentStoreOp

--- src/forvulkan_parameters.f90
+++ src/forvulkan_parameters.f90
@@ -3919,4 +3919,19 @@ module forvulkan_parameters
   integer(c_int32_t), parameter, public :: VK_ATTACHMENT_LOAD_OP_NONE_EXT = VK_ATTACHMENT_LOAD_OP_NONE_KHR
 
 
+  !? VkAttachmentStoreOp.
+
+
+  integer(c_int32_t), parameter, public :: VK_ATTACHMENT_STORE_OP_STORE = 0
+  integer(c_int32_t), parameter, public :: VK_ATTACHMENT_STORE_OP_DONT_CARE = 1
+  ! Provided by VK_VERSION_1_3
+  integer(c_int32_t), parameter, public :: VK_ATTACHMENT_STORE_OP_NONE = 1000301000
+  ! Provided by VK_KHR_dynamic_rendering VK_KHR_load_store_op_none
+  integer(c_int32_t), parameter, public :: VK_ATTACHMENT_STORE_OP_NONE_KHR = VK_ATTACHMENT_STORE_OP_NONE
+  ! Provided by VK_QCOM_render_pass_store_ops
+  integer(c_int32_t), parameter, public :: VK_ATTACHMENT_STORE_OP_NONE_QCOM = VK_ATTACHMENT_STORE_OP_NONE
+  ! Provided by VK_EXT_load_store_op_none
+  integer(c_int32_t), parameter, public :: VK_ATTACHMENT_STORE_OP_NONE_EXT = VK_ATTACHMENT_STORE_OP_NONE
+
+
 end module forvulkan_parameters
