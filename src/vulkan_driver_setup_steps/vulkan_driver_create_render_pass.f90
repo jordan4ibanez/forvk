1bc0c6043c5fcd52d26cf46f754ac330020e213e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-19 09:44:54 -0500

    This needs to be added here

--- src/vulkan_driver_setup_steps/vulkan_driver_create_render_pass.f90
+++ src/vulkan_driver_setup_steps/vulkan_driver_create_render_pass.f90
@@ -20,6 +20,7 @@ contains
     type(vk_attachment_reference), target :: color_attachment_reference
     type(vk_subpass_description_t), target :: subpass
     type(vk_render_pass_create_info), target  :: render_pass_create_info
+    type(vk_subpass_dependency), target :: dependency
 
     color_attachment_description%format = swapchain_image_format
     color_attachment_description%samples = VK_SAMPLE_COUNT_1_BIT
@@ -37,11 +38,20 @@ contains
     subpass%color_attachment_count = 1
     subpass%p_color_attachments = c_loc(color_attachment_reference)
 
+    dependency%src_subpass = VK_SUBPASS_EXTERNAL
+    dependency%dst_subpass = 0
+    dependency%src_stage_mask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT
+    dependency%src_access_mask = 0
+    dependency%dst_stage_mask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT
+    dependency%dst_access_mask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT
+
     render_pass_create_info%s_type = VK_STRUCTURE_TYPE%RENDER_PASS_CREATE_INFO
     render_pass_create_info%attachment_count = 1
     render_pass_create_info%p_attachments = c_loc(color_attachment_description)
     render_pass_create_info%subpass_count = 1
     render_pass_create_info%p_subpasses = c_loc(subpass)
+    render_pass_create_info%dependency_count = 1
+    render_pass_create_info%p_dependencies = c_loc(dependency)
 
     if (vk_create_render_pass(logical_device, c_loc(render_pass_create_info), c_null_ptr, render_pass) /= VK_SUCCESS) then
       error stop "[Vulkan] Error: Failed to create render pass."
