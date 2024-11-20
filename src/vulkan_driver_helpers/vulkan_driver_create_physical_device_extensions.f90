9f6e631cfefff6d03bc4bc6c2770032ee88f377f
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-20 04:19:46 -0500

    Another friendly gc yet again

--- src/vulkan_driver_helpers/vulkan_driver_create_physical_device_extensions.f90
+++ src/vulkan_driver_helpers/vulkan_driver_create_physical_device_extensions.f90
@@ -3,6 +3,7 @@ module vulkan_driver_create_physical_device_extensions
   use :: vector
   use :: forvulkan
   use :: forvulkan_parameters
+  use :: string_f90
   implicit none
 
 
@@ -28,6 +29,22 @@ contains
 
     call required_device_extensions%shrink_to_fit()
   end subroutine create_required_physical_device_extensions
+
+
+  subroutine device_extensions_vec_gc(raw_c_ptr_ptr)
+    implicit none
+
+    type(c_ptr), intent(in), value :: raw_c_ptr_ptr
+    type(c_ptr), pointer :: raw_c_ptr
+    character(len = :, kind = c_char), pointer :: str
+
+    print*,"hello from required physical extensions gc"
+
+    call c_f_pointer(raw_c_ptr_ptr, raw_c_ptr)
+    str => string_from_c(raw_c_ptr)
+
+    deallocate(str)
+  end subroutine device_extensions_vec_gc
 
 
 end module vulkan_driver_create_physical_device_extensions
