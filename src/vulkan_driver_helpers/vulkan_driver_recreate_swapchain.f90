e0ac75b1dd4569b9614791b630d403650492caf2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-21 04:42:37 -0500

    Create vulkan_driver_recreate_swapchain.f90

---
+++ src/vulkan_driver_helpers/vulkan_driver_recreate_swapchain.f90
@@ -0,0 +1,32 @@
+module vulkan_driver_recreate_swapchain
+  use, intrinsic :: iso_c_binding
+  use :: forvulkan
+  use :: forvulkan_parameters
+  implicit none
+
+
+  private
+
+
+  public :: recreate_swapchain
+
+
+contains
+
+
+  subroutine recreate_swapchain()
+    implicit none
+
+    call clean_up_swapchain()
+
+
+  end subroutine recreate_swapchain
+
+
+  subroutine clean_up_swapchain()
+    implicit none
+
+  end subroutine clean_up_swapchain
+
+
+end module vulkan_driver_recreate_swapchain
