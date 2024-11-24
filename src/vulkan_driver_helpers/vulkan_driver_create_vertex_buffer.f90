1ce895e7b1b0f22426d836ab88b3ff58c3ccea36
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-23 19:10:46 -0500

    yada yada

--- src/vulkan_driver_helpers/vulkan_driver_create_vertex_buffer.f90
+++ src/vulkan_driver_helpers/vulkan_driver_create_vertex_buffer.f90
@@ -18,9 +18,9 @@ contains
     type(vk_buffer), intent(inout) :: vertex_buffer
     type(vk_device_memory), intent(inout) :: vertex_buffer_memory
     type(vk_buffer_create_info), target :: buffer_info
-    type(vk_memory_allocate_info), target :: alloc_info
     type(vk_memory_requirements), target :: mem_requirements
     type(vk_physical_device_memory_properties), target :: mem_properties
+    type(vk_memory_allocate_info), target :: alloc_info
 
     buffer_info%s_type = VK_STRUCTURE_TYPE%BUFFER_CREATE_INFO
     buffer_info%size = sizeof(vertex_data(1)) * size(vertex_data)
@@ -34,9 +34,17 @@ contains
     call vk_get_buffer_memory_requirements(logical_device, vertex_buffer, c_loc(mem_requirements))
 
     alloc_info%s_type = VK_STRUCTURE_TYPE%MEMORY%ALLOCATE_INFO
-    alloc_info%allocation_size =
+    alloc_info%allocation_size = mem_requirements%size
+    alloc_info%memory_type_index = find_memory_type(physical_device, mem_requirements%memory_type_bits, ior(VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT, VK_MEMORY_PROPERTY_HOST_COHERENT_BIT))
 
+    if (vk_allocate_memory(logical_device, c_loc(alloc_info), c_null_ptr, vertex_buffer_memory) /= VK_SUCCESS) then
+      error stop "[Vulkan] Error: Failed to allocate vertex buffer memory."
+    end if
 
+    ! todo: put this thing into a more generic function or module, lol. This is horrible.
+    if (vk_bind_buffer_memory(logical_device, vertex_buffer, vertex_buffer_memory, 0_8) /= VK_SUCCESS) then
+      error stop "[Vulkan] Error: Failed to bind vertex buffer memory."
+    end if
   end subroutine create_vertex_buffer
 
 
