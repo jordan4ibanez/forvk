d46396d714ff385767422d4bbb37fce0c97f89de
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-24 13:00:08 -0500

    overhaul

--- src/vulkan_driver_helpers/vulkan_driver_create_index_buffer.f90
+++ src/vulkan_driver_helpers/vulkan_driver_create_index_buffer.f90
@@ -18,24 +18,31 @@ contains
     type(vk_device_size) :: buffer_size
     type(vk_buffer) :: staging_buffer
     type(vk_device_memory) :: staging_buffer_memory
+    ! void *
+    type(c_ptr) :: data
 
     buffer_size%data = sizeof(indices(1)) * size(indices)
 
     call create_buffer(physical_device, logical_device, buffer_size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, ior(VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT, VK_MEMORY_PROPERTY_HOST_COHERENT_BIT), staging_buffer, staging_buffer_memory)
 
+    if (vk_map_memory(logical_device, staging_buffer_memory, vk_device_size(0_8), buffer_size, 0, data) /= VK_SUCCESS) then
+      error stop "[Vulkan] Error: Failed to map index buffer memory."
+    end if
+
 
   end subroutine create_index_buffer
 
 
-  subroutine totally_not_memcpy_index(data, vertices)
+  subroutine totally_not_memcpy_indices(data, indices)
     implicit none
 
     type(c_ptr), intent(in), value :: data
-    type(vertex), dimension(:), intent(in) :: vertices
-    type(vertex), dimension(:), pointer :: data_in_buffer
+    integer(c_int32_t), dimension(:), intent(in) :: indices
+    integer(c_int32_t), dimension(:), pointer :: data_in_buffer
 
-    call c_f_pointer(data, data_in_buffer, [size(vertices)])
-    data_in_buffer = vertices
-  end subroutine totally_not_memcpy_index
+    call c_f_pointer(data, data_in_buffer, [size(indices)])
+    data_in_buffer = indices
+  end subroutine totally_not_memcpy_indices
+
 
 end module vulkan_driver_create_index_buffer
