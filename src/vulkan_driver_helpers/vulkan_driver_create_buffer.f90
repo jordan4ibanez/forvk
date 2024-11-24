566dc8595f06040dbaa2a4a57c3d6fc787490f0b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-24 06:39:18 -0500

    time for LE PAIN

---
+++ src/vulkan_driver_helpers/vulkan_driver_create_buffer.f90
@@ -0,0 +1,14 @@
+module vulkan_driver_create_buffer
+  use, intrinsic :: iso_c_binding
+  use :: forvulkan
+  implicit none
+
+contains
+
+
+  subroutine create_buffer()
+
+  end subroutine create_buffer
+
+
+end module vulkan_driver_create_buffer
