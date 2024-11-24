029ca470e1cae33a35fcf3e706318499116c4760
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-24 12:01:46 -0500

    we will pull this out for now

--- src/vulkan_driver_helpers/vulkan_driver_create_vertex_buffer.f90
+++ src/vulkan_driver_helpers/vulkan_driver_create_vertex_buffer.f90
@@ -27,18 +27,25 @@ contains
 
     buffer_size = sizeof(vertices(1)) * size(vertices)
 
-    ! call create_buffer(physical_device, logical_device, buffer_size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, ior(VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT, VK_MEMORY_PROPERTY_HOST_COHERENT_BIT), staging_buffer, staging_buffer_memory)
+    call create_buffer(physical_device, logical_device, buffer_size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, ior(VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT, VK_MEMORY_PROPERTY_HOST_COHERENT_BIT), staging_buffer, staging_buffer_memory)
 
-    call create_buffer(physical_device, logical_device, buffer_size, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, ior(VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT, VK_MEMORY_PROPERTY_HOST_COHERENT_BIT), vertex_buffer, vertex_buffer_memory)
-
-    if (vk_map_memory(logical_device, vertex_buffer_memory, 0_8, buffer_size, 0, data) /= VK_SUCCESS) then
+    if (vk_map_memory(logical_device, staging_buffer_memory, 0_8, buffer_size, 0, data) /= VK_SUCCESS) then
       error stop "[Vulkan] Error: Failed to map memory."
     end if
 
     call totally_not_memcpy(data, vertices)
 
-    call vk_unmap_memory(logical_device, vertex_buffer_memory)
+    call vk_unmap_memory(logical_device, staging_buffer_memory)
+
+    call create_buffer(physical_device, logical_device, buffer_size, ior(VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT), VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, vertex_buffer, vertex_buffer_memory)
+
   end subroutine create_vertex_buffer
+
+
+  subroutine copy_buffer()
+    implicit none
+
+  end subroutine
 
 
   subroutine totally_not_memcpy(data, vertices)
@@ -51,6 +58,50 @@ contains
     call c_f_pointer(data, data_in_buffer, [size(vertices)])
     data_in_buffer = vertices
   end subroutine totally_not_memcpy
+
+
+
+  subroutine create_buffer(physical_device, logical_device, buffer_size, usage, properties, buffer, buffer_memory)
+    implicit none
+
+    type(vk_physical_device), intent(in), value :: physical_device
+    type(vk_device), intent(in), value :: logical_device
+    ! VkBufferUsageFlags
+    integer(c_int32_t), intent(in), value :: usage
+    ! VkMemoryPropertyFlags
+    integer(c_int32_t), intent(in), value :: properties
+    type(vk_buffer), intent(inout) :: buffer
+    type(vk_device_memory), intent(inout) :: buffer_memory
+    type(vk_buffer_create_info), target :: buffer_info
+    ! VkDeviceSize
+    integer(c_int64_t), intent(in), value :: buffer_size
+    type(vk_memory_allocate_info), target :: alloc_info
+    type(vk_memory_requirements), target :: mem_requirements
+
+    buffer_info%s_type = VK_STRUCTURE_TYPE%BUFFER_CREATE_INFO
+    buffer_info%size = buffer_size
+    buffer_info%usage = usage
+    buffer_info%sharing_mode = VK_SHARING_MODE_EXCLUSIVE
+
+    if (vk_create_buffer(logical_device, c_loc(buffer_info), c_null_ptr, buffer) /= VK_SUCCESS) then
+      error stop "[Vulkan] Error: Failed to create buffer."
+    end if
+
+    call vk_get_buffer_memory_requirements(logical_device, buffer, c_loc(mem_requirements))
+
+    alloc_info%s_type = VK_STRUCTURE_TYPE%MEMORY%ALLOCATE_INFO
+    alloc_info%allocation_size = mem_requirements%size
+    alloc_info%memory_type_index = find_memory_type(physical_device, mem_requirements%memory_type_bits, properties)
+
+    if (vk_allocate_memory(logical_device, c_loc(alloc_info), c_null_ptr, buffer_memory) /= VK_SUCCESS) then
+      error stop "[Vulkan] Error: Failed to allocate buffer memory."
+    end if
+
+    ! todo: put this thing into a more generic function or module, lol. This is horrible.
+    if (vk_bind_buffer_memory(logical_device, buffer, buffer_memory, 0_8) /= VK_SUCCESS) then
+      error stop "[Vulkan] Error: Failed to bind buffer memory."
+    end if
+  end subroutine create_buffer
 
 
 end module vulkan_driver_create_vertex_buffer
