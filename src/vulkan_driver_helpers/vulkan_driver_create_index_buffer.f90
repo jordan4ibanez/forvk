e40b2b30573c4de0483089e5b38b4636e67c9dd2
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-24 12:57:19 -0500

    dump this thing in

--- src/vulkan_driver_helpers/vulkan_driver_create_index_buffer.f90
+++ src/vulkan_driver_helpers/vulkan_driver_create_index_buffer.f90
@@ -9,9 +9,11 @@ module vulkan_driver_create_index_buffer
 contains
 
 
-  subroutine create_index_buffer(indices)
+  subroutine create_index_buffer(physical_device, logical_device, indices)
     implicit none
 
+    type(vk_physical_device), intent(in), value :: physical_device
+    type(vk_device), intent(in), value :: logical_device
     integer(c_int32_t), dimension(:) :: indices
     type(vk_device_size) :: buffer_size
     type(vk_buffer) :: staging_buffer
@@ -19,10 +21,21 @@ contains
 
     buffer_size%data = sizeof(indices(1)) * size(indices)
 
-
-
+    call create_buffer(physical_device, logical_device, buffer_size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, ior(VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT, VK_MEMORY_PROPERTY_HOST_COHERENT_BIT), staging_buffer, staging_buffer_memory)
 
 
   end subroutine create_index_buffer
+
+
+  subroutine totally_not_memcpy_index(data, vertices)
+    implicit none
+
+    type(c_ptr), intent(in), value :: data
+    type(vertex), dimension(:), intent(in) :: vertices
+    type(vertex), dimension(:), pointer :: data_in_buffer
+
+    call c_f_pointer(data, data_in_buffer, [size(vertices)])
+    data_in_buffer = vertices
+  end subroutine totally_not_memcpy_index
 
 end module vulkan_driver_create_index_buffer
