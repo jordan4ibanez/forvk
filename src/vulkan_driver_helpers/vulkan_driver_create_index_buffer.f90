1de29f8f2de53c2105df6b2c03c5ea7f3fd3d46b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-24 12:53:30 -0500

    Now pull those in

--- src/vulkan_driver_helpers/vulkan_driver_create_index_buffer.f90
+++ src/vulkan_driver_helpers/vulkan_driver_create_index_buffer.f90
@@ -1,5 +1,7 @@
 module vulkan_driver_create_index_buffer
   use, intrinsic :: iso_c_binding
+  use :: vulkan_driver_create_buffer
+  use :: vulkan_driver_copy_buffer
   use :: forvulkan
   implicit none
 
@@ -7,8 +9,19 @@ module vulkan_driver_create_index_buffer
 contains
 
 
-  subroutine create_index_buffer()
+  subroutine create_index_buffer(indices)
     implicit none
+
+    integer(c_int32_t), dimension(:) :: indices
+    type(vk_device_size) :: buffer_size
+    type(vk_buffer) :: staging_buffer
+    type(vk_device_memory) :: staging_buffer_memory
+
+    buffer_size%data = sizeof(indices(1)) * size(indices)
+
+
+
+
 
   end subroutine create_index_buffer
 
