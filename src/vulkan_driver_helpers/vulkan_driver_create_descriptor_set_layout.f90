78ab0731aa45246fdaec2f9fa494405d236d608a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-25 11:44:27 -0500

    Add this

--- src/vulkan_driver_helpers/vulkan_driver_create_descriptor_set_layout.f90
+++ src/vulkan_driver_helpers/vulkan_driver_create_descriptor_set_layout.f90
@@ -10,6 +10,14 @@ contains
   subroutine create_descriptor_set_layout()
     implicit none
 
+    type(vk_descriptor_set_layout_binding), target :: ubo_layout_binding
+
+    ubo_layout_binding%binding = 0
+    ubo_layout_binding%descriptor_type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER
+    ubo_layout_binding%descriptor_count = 1
+    ubo_layout_binding%stage_flags = VK_SHADER_STAGE_VERTEX_BIT
+    ubo_layout_binding%p_immutable_samplers = c_null_ptr
+    
 
   end subroutine create_descriptor_set_layout
 
