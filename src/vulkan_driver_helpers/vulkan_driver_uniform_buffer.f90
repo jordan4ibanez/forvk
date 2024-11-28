ac21137456b200e4633145e2f0b8049fe0126498
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-28 02:29:47 -0500

    space this

--- src/vulkan_driver_helpers/vulkan_driver_uniform_buffer.f90
+++ src/vulkan_driver_helpers/vulkan_driver_uniform_buffer.f90
@@ -4,10 +4,12 @@ module vulkan_driver_uniform_buffer
   use :: matrix_4f
   implicit none
 
+
   type :: uniform_buffer_object
     type(mat4f) :: model
     type(mat4f) :: view
     type(mat4f) :: proj
   end type uniform_buffer_object
+
 
 end module vulkan_driver_uniform_buffer
