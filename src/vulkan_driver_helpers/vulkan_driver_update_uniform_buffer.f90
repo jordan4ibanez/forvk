535cc2d2c1063a7039392eac6652e20a7eaa5a20
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-11-28 03:49:46 -0500

    time to do things and such

--- src/vulkan_driver_helpers/vulkan_driver_update_uniform_buffer.f90
+++ src/vulkan_driver_helpers/vulkan_driver_update_uniform_buffer.f90
@@ -1,8 +1,12 @@
 module vulkan_driver_update_uniform_buffer
   use, intrinsic :: iso_c_binding
   use :: forvulkan
+  use :: matrix_4f
+  use :: vulkan_driver_uniform_buffer
   implicit none
 
+  ! I don't feel like bringing in vector so this will just run as fast as it will.
+  real(c_float) :: time = 0.0
 
 contains
 
@@ -11,6 +15,12 @@ contains
     implicit none
 
     integer(c_int64_t), intent(in), value :: current_image
+    type(uniform_buffer_object), pointer :: ubo_pointer
+
+    time = time + 0.0001
+
+    ! todo: do things here and stuff.
+
 
   end subroutine update_uniform_buffer
 
