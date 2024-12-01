70bf1da4f7d75aa4e94cfeee54ead5ebaa36f68e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-01 14:16:49 -0500

    Create vulkan_driver_create_descriptor_sets.f90

---
+++ src/vulkan_driver_helpers/vulkan_driver_create_descriptor_sets.f90
@@ -0,0 +1,18 @@
+module vulkan_driver_create_descriptor_sets
+  use, intrinsic :: iso_c_binding
+  use :: forvulkan
+  implicit none
+
+
+contains
+
+
+  subroutine create_descriptor_sets()
+    implicit none
+
+    
+
+  end subroutine create_descriptor_sets
+
+
+end module vulkan_driver_create_descriptor_sets
