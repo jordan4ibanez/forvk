b17e1c26d72c1a972d595343041a0c89bb939a11
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-10 04:14:07 -0500

    Create vulkan_uniform_buffer.f90

---
+++ src/vulkan_driver_helpers/vulkan_uniform_buffer.f90
@@ -0,0 +1,14 @@
+module vulkan_uniform_buffer
+  use, intrinsic :: iso_c_binding
+  use :: forvulkan
+  use :: matrix_4f
+  implicit none
+
+
+  type :: uniform_buffer_object
+    type(mat4f) :: camera_matrix
+    type(mat4f) :: object_matrix
+  end type uniform_buffer_object
+
+
+end module vulkan_uniform_buffer
