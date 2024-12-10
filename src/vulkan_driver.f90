87520075b1eef1ee756613a93f62b67a2d90f012
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-10 03:57:21 -0500

    Update vulkan_driver.f90

--- src/vulkan_driver.f90
+++ src/vulkan_driver.f90
@@ -111,6 +111,7 @@ module vulkan_driver
     procedure :: create_vertex_buffer => vk_driver_create_vertex_buffer
     procedure :: create_buffer => vk_driver_create_buffer
     procedure :: find_memory_type => vk_driver_find_memory_type
+    procedure :: copy_buffer => vk_driver_copy_buffer
   end type vk_driver
 
 
@@ -1817,7 +1818,7 @@ contains
     call vk_unmap_memory(this%logical_device, staging_buffer_memory)
 
     call this%create_buffer(buffer_size, ior(VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT), VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, this%vertex_buffer, this%vertex_buffer_memory)
-    call copy_buffer(this%logical_device, staging_buffer, this%vertex_buffer, buffer_size, this%command_pool, this%graphics_queue)
+    call this%copy_buffer(staging_buffer, this%vertex_buffer, buffer_size)
 
     call vk_destroy_buffer(this%logical_device, staging_buffer, c_null_ptr)
     call vk_free_memory(this%logical_device, staging_buffer_memory, c_null_ptr)
@@ -1898,6 +1899,60 @@ contains
 
     error stop "[Vulkan] Error: Failed to find suitable memory type."
   end function vk_driver_find_memory_type
+
+
+  subroutine vk_driver_copy_buffer(this, src_buffer, dst_buffer, buffer_size)
+    implicit none
+
+    class(vk_driver), intent(inout) :: this
+    type(vk_buffer), intent(in), value :: src_buffer, dst_buffer
+    type(vk_device_size), intent(in), value :: buffer_size
+    type(vk_command_buffer_allocate_info), target :: alloc_info
+    type(vk_command_buffer), target :: command_buffer
+    type(vk_command_buffer_begin_info), target :: begin_info
+    type(vk_buffer_copy), target :: copy_region
+    type(vk_submit_info), target :: submit_info
+
+    alloc_info%s_type = VK_STRUCTURE_TYPE%COMMAND_BUFFER_ALLOCATE_INFO
+    alloc_info%level = VK_COMMAND_BUFFER_LEVEL_PRIMARY
+    alloc_info%command_pool = this%command_pool
+    alloc_info%command_buffer_count = 1
+
+    if (vk_allocate_command_buffers(this%logical_device, c_loc(alloc_info), c_loc(command_buffer)) /= VK_SUCCESS) then
+      error stop "[Vulkan] Error: Failed to allocate command buffer."
+    end if
+
+    begin_info%s_type = VK_STRUCTURE_TYPE%COMMAND_BUFFER_BEGIN_INFO
+    begin_info%flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT
+
+    if (vk_begin_command_buffer(command_buffer, c_loc(begin_info)) /= VK_SUCCESS) then
+      error stop "[Vulkan] Error: Failed to start command buffer."
+    end if
+
+    copy_region%src_offset%data = 0
+    copy_region%dst_offset%data = 0
+    copy_region%size = buffer_size
+
+    call vk_cmd_copy_buffer(command_buffer, src_buffer, dst_buffer, 1, c_loc(copy_region))
+
+    if (vk_end_command_buffer(command_buffer) /= VK_SUCCESS) then
+      error stop "[Vulkan] Error: Failed to end command buffer."
+    end if
+
+    submit_info%s_type = VK_STRUCTURE_TYPE%SUBMIT_INFO
+    submit_info%command_buffer_count = 1
+    submit_info%p_command_buffers = c_loc(command_buffer)
+
+    if (vk_queue_submit(this%graphics_queue, 1, c_loc(submit_info), vk_fence(VK_NULL_HANDLE)) /= VK_SUCCESS) then
+      error stop "[Vulkan] Error: Failed to submit queue."
+    end if
+
+    if (vk_queue_wait_idle(this%graphics_queue) /= VK_SUCCESS) then
+      error stop "[Vulkan] Error: Failed to wait idle on queue."
+    end if
+
+    call vk_free_command_buffers(this%logical_device, this%command_pool, 1, c_loc(command_buffer))
+  end subroutine vk_driver_copy_buffer
 
 
 
