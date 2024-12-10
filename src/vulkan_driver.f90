9942bc77c9ef6f2ff1d76b26e581d9a0a755ca27
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-10 00:30:08 -0500

    Update vulkan_driver.f90

--- src/vulkan_driver.f90
+++ src/vulkan_driver.f90
@@ -102,6 +102,7 @@ module vulkan_driver
     procedure :: query_swapchain_support => vk_driver_query_swapchain_support
     procedure :: create_swapchain => vk_driver_create_swapchain
     procedure :: create_image_views => vk_driver_create_image_views
+    procedure :: create_render_pass => vk_driver_create_render_pass
   end type vk_driver
 
 
@@ -164,7 +165,7 @@ contains
 
     call this%create_image_views()
 
-    ! call create_render_pass(logical_device, render_pass, swapchain_image_format)
+    call this%create_render_pass()
 
     ! call create_descriptor_set_layout(logical_device, descriptor_set_layout)
 
@@ -1488,5 +1489,51 @@ contains
     end do
   end subroutine vk_driver_create_image_views
 
+
+  subroutine vk_driver_create_render_pass(this)
+    implicit none
+
+    class(vk_driver), intent(inout) :: this
+    type(vk_attachment_description), target :: color_attachment_description
+    type(vk_attachment_reference), target :: color_attachment_reference
+    type(vk_subpass_description_t), target :: subpass
+    type(vk_render_pass_create_info), target  :: render_pass_create_info
+    type(vk_subpass_dependency), target :: dependency
+
+    color_attachment_description%format = this%swapchain_image_format
+    color_attachment_description%samples = VK_SAMPLE_COUNT_1_BIT
+    color_attachment_description%load_op = VK_ATTACHMENT_LOAD_OP_CLEAR
+    color_attachment_description%store_op = VK_ATTACHMENT_STORE_OP_STORE
+    color_attachment_description%stencil_load_op = VK_ATTACHMENT_LOAD_OP_DONT_CARE
+    color_attachment_description%stencil_store_op = VK_ATTACHMENT_STORE_OP_DONT_CARE
+    color_attachment_description%initial_layout = VK_IMAGE_LAYOUT_UNDEFINED
+    color_attachment_description%final_layout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR
+
+    color_attachment_reference%attachment = 0
+    color_attachment_reference%layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL
+
+    subpass%pipeline_bind_point = VK_PIPELINE_BIND_POINT_GRAPHICS
+    subpass%color_attachment_count = 1
+    subpass%p_color_attachments = c_loc(color_attachment_reference)
+
+    dependency%src_subpass = VK_SUBPASS_EXTERNAL
+    dependency%dst_subpass = 0
+    dependency%src_stage_mask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT
+    dependency%src_access_mask = 0
+    dependency%dst_stage_mask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT
+    dependency%dst_access_mask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT
+
+    render_pass_create_info%s_type = VK_STRUCTURE_TYPE%RENDER_PASS_CREATE_INFO
+    render_pass_create_info%attachment_count = 1
+    render_pass_create_info%p_attachments = c_loc(color_attachment_description)
+    render_pass_create_info%subpass_count = 1
+    render_pass_create_info%p_subpasses = c_loc(subpass)
+    render_pass_create_info%dependency_count = 1
+    render_pass_create_info%p_dependencies = c_loc(dependency)
+
+    if (vk_create_render_pass(this%logical_device, c_loc(render_pass_create_info), c_null_ptr, this%render_pass) /= VK_SUCCESS) then
+      error stop "[Vulkan] Error: Failed to create render pass."
+    end if
+  end subroutine vk_driver_create_render_pass
 
 end module vulkan_driver
