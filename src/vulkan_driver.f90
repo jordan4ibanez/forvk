2303d0bdab65ff2f9f519e7a6553c563ff245808
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-12-11 21:40:22 -0500

    begin_single_time_commands

--- src/vulkan_driver.f90
+++ src/vulkan_driver.f90
@@ -130,6 +130,7 @@ module vulkan_driver
     procedure :: recreate_swapchain => vk_driver_recreate_swapchain
     procedure :: create_texture_image => vk_driver_create_texture_image
     procedure :: create_image => vk_driver_create_image
+    procedure :: begin_single_time_commands => vk_driver_begin_single_time_commands
   end type vk_driver
 
 
@@ -2518,5 +2519,30 @@ contains
     data_in_buffer = texture_data
   end subroutine totally_not_memcpy_texture
 
+
+  function vk_driver_begin_single_time_commands(this) result(command_buffer)
+    implicit none
+
+    class(vk_driver), intent(inout) :: this
+    type(vk_command_buffer), target :: command_buffer
+    type(vk_command_buffer_allocate_info), target :: alloc_info
+    type(vk_command_buffer_begin_info), target :: begin_info
+
+    alloc_info%s_type = VK_STRUCTURE_TYPE%COMMAND_BUFFER_ALLOCATE_INFO
+    alloc_info%level = VK_COMMAND_BUFFER_LEVEL_PRIMARY
+    alloc_info%command_pool = this%command_pool
+    alloc_info%command_buffer_count = 1
+
+    if (vk_allocate_command_buffers(this%logical_device, c_loc(alloc_info), c_loc(command_buffer)) /= VK_SUCCESS) then
+      error stop "[Vulkan] Error: Failed to allocate command buffers."
+    end if
+
+    begin_info%s_type = VK_STRUCTURE_TYPE%COMMAND_BUFFER_BEGIN_INFO
+    begin_info%flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT
+
+    if (vk_begin_command_buffer(command_buffer, c_loc(begin_info)) /= VK_SUCCESS) then
+      error stop "[Vulkan] Error: Failed to begin command buffer."
+    end if
+  end function vk_driver_begin_single_time_commands
 
 end module vulkan_driver
